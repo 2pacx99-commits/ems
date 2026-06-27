d8c6f8cc135113f38c14fc9850e31cdfb410900b
2pacx99-commits <2pac.x99@gmail.com>
2026-06-27 09:36:02 +0300

    Delete zip-project.js

--- zip-project.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const archiver = require('archiver');
-const output = fs.createWriteStream('EMS-Project.zip');
-const archive = archiver('zip', { zlib: { level: 9 } });
-output.on('close', () => console.log('✅ تم إنشاء ملف EMS-Project.zip بنجاح، وهو جاهز للرفع على GitHub / Vercel.'));
-archive.pipe(output);
-archive.glob('**/*', { ignore: ['node_modules/**', 'setup.js', 'zip-project.js', 'EMS-Project.zip'] });
-archive.finalize();
